58d81325f44591044f6f06c3bdcaaf705f3bf1ac
zhugefei <yshjtdl1990@qq.com>
2016-02-16 18:30:25 +0800

    初始化
    初始化项目

---
+++ server/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  server
+//
+//  Created by zhugefei on 16-2-16.
+//  Copyright (c) 2016年 zhugefei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
